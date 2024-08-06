7dd5089151c88871f10e13bdab863701fc8620ae
MohammadJilan <MohammadJilanmu@gmail.com>
2024-08-06 17:39:00 +0800

    constraint1

--- Tables/dbo.test2.sql
+++ Tables/dbo.test2.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbo].[test2] (
   [food] [varchar](255) NOT NULL,
   [rating] [varchar](255) NULL,
-  PRIMARY KEY CLUSTERED ([food])
+  PRIMARY KEY CLUSTERED ([food]),
+  CONSTRAINT [check_rating] CHECK ([rating]>=(0) AND [rating]<=(10))
 )
 ON [PRIMARY]
 GO
