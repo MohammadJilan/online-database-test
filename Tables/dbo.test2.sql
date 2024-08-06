61867e59856021a2675d8d0ac0fd215aeb75a381
MohammadJilan <MohammadJilanmu@gmail.com>
2024-08-06 17:43:09 +0800

    a

--- Tables/dbo.test2.sql
+++ Tables/dbo.test2.sql
@@ -1,8 +1,7 @@
 ﻿CREATE TABLE [dbo].[test2] (
   [food] [varchar](255) NOT NULL,
   [rating] [varchar](255) NULL,
-  PRIMARY KEY CLUSTERED ([food]),
-  CONSTRAINT [check_food] CHECK (NOT [food] like '%[0-9]%')
+  PRIMARY KEY CLUSTERED ([food])
 )
 ON [PRIMARY]
 GO
