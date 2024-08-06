d5cb2ce8596d81340e78c1c9409cc432ab1b01f7
MohammadJilan <MohammadJilanmu@gmail.com>
2024-08-06 17:49:00 +0800

    .

---
+++ Tables/dbo.student.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[student] (
+  [studentID] [int] IDENTITY,
+  [Name] [varchar](50) NULL,
+  CONSTRAINT [PK_student_studentID] PRIMARY KEY CLUSTERED ([studentID])
+)
+ON [PRIMARY]
+GO
